bd5200e33d769e46789eb34ede8ddcc318e33630
mai-yoshida <hudiezhimu@gmail.com>
2022-08-03 22:33:25 +0900

    ビューファイルの編集

--- app/controllers/edais_controller.rb
+++ app/controllers/edais_controller.rb
@@ -1,7 +1,6 @@
 class EdaisController < ApplicationController
   def index
     @edais = Edai.order(entry_day: "ASC")
-    #@edai_show = Edai.find(params[:id])
     @edai_sum_balance = Edai.group(:balance_id).sum(:price)
     @edai_sum = Edai.group(:genre_id).sum(:price)
     @edai_sum_description = Edai.group(:description_id).sum(:price)
